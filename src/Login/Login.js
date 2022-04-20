682bda5cc5dc2ae9068b8860c4e246591d553a7e
YoadKaspi <yoad.kas88@gmail.com>
2022-04-20 16:07:20 +0300

    Co-authored-by: EladBlaier <EladBlaier@users.noreply.github.com>

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,67 +1,99 @@
-import users from '../Users.js';
-import React, {useState} from 'react';
-import { useSate, useEffect } from "react";
-import { Link, useNavigate } from 'react-router-dom';
-import Register from '../Register/Register';
+import users from "../Users.js";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const navigate = useNavigate();
 
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLogin  = () => {
+    const handleLogin = () => {
         var flag = false;
         for (var i in users) {
-            if(users[i].username === userName && users[i].password === password) {
+            if (users[i].username === userName && users[i].password === password) {
                 flag = true;
                 break;
             }
         }
-        if(!flag) {
-            document.getElementById('unregistered').innerHTML = "username or password are incorrect";
+        if (!flag) {
+            document.getElementById("unregistered").innerHTML = "username or password are incorrect";
+        } else {
+            navigate("/register");
         }
-    }
+    };
 
     const handleClear = () => {
-        setUserName('');
-        setPassword('');
-    }
+        setUserName("");
+        setPassword("");
+    };
 
     return (
-        <form name='LoginForm'>
+        <form name="LoginForm">
             <div className="field-name d-grid gap-0.8 central-frame">
                 <div>
-                    <label htmlFor="exampleFormControlInput1" className="form-label head-line">Welcome To Brawlhalla</label><br/>
+                    <label htmlFor="exampleFormControlInput1" className="form-label head-line">
+                        Welcome To Brawlhalla
+                    </label>
+                    <br />
                 </div>
                 <div className="mb-3">
                     <div className="row">
                         <div className="col-sm-3">
-                            <label htmlFor="exampleFormControlInput1" className="form-label field-label">Username</label><br/>
+                            <label htmlFor="exampleFormControlInput1" className="form-label field-label">
+                                Username
+                            </label>
+                            <br />
                         </div>
                     </div>
-                    <input type="text" className="form-control" id="username" value={userName} placeholder="My Username" onChange={e => setUserName(e.target.value)}></input>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        value={userName}
+                        placeholder="My Username"
+                        onChange={(e) => setUserName(e.target.value)}
+                    ></input>
                 </div>
                 <div className="mb-3">
                     <div className="row">
                         <div className="col-sm-3">
-                            <label htmlFor="exampleFormControlInput1" className="form-label field-label">Password</label><br/>
+                            <label htmlFor="exampleFormControlInput1" className="form-label field-label">
+                                Password
+                            </label>
+                            <br />
                         </div>
                     </div>
-                    <input type="password" className="form-control" id="psw" value={password} rows="3" placeholder='Password' onChange={e => setPassword(e.target.value)}></input>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="psw"
+                        value={password}
+                        rows="3"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
                 </div>
                 <div>
-                    <label id='unregistered' className='form-label validation-label'></label>
+                    <label id="unregistered" className="form-label validation-label"></label>
                 </div>
                 <div>
-                    <button type="button" className="btn btn-link" >Not registered yet? register here</button>
+                    <button type="button" className="btn btn-link" onClick={() => navigate("/register")}>
+                        Not registered yet? register here
+                    </button>
                 </div>
                 <div className="d-grid gap-2">
-                    <button className="btn btn-outline-success" type="button" onClick={()=>handleLogin()}>Log in</button>
-                    <button type="button" className="btn btn-outline-secondary" onClick={()=>handleClear()}>Clear</button>
+                    <button className="btn btn-outline-success" type="button" onClick={() => handleLogin()}>
+                        Log in
+                    </button>
+                    <button type="button" className="btn btn-outline-secondary" onClick={() => handleClear()}>
+                        Clear
+                    </button>
                 </div>
             </div>
         </form>
     );
-};
+}
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Register from './Register/Register';
-import Login from  './Login/Login'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Register from "./Register/Register";
+import Login from "./Login/Login";
+import MainChat from "./Chat/MainChat";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/mainchat" element={<MainChat />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
