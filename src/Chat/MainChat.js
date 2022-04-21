05fe27505874c513ab8831bd2a86fbd346a78524
EladBlaier <elad.blaier@gmail.com>
2022-04-21 17:52:07 +0300

    fixed contact list

--- src/Chat/MainChat.js
+++ src/Chat/MainChat.js
@@ -5,36 +5,25 @@ import RenderContact from "./Contact/Contact";
 import { useParams } from "react-router-dom";
 
 function MainChat() {
-    /*var messages = msgList.map((message, key) => {
-        return <Message {...message} key={key} />;
-    });*/
-    const {userName} = useParams();
-    console.log(userName);
-    let messages = [];
-    for (var msg in msgList) {
-        if(msg.sender === userName) {
-            console.log("success");
-            messages.push(msg);
-        }
+  const { userName } = useParams();
+  var renderedMessages = msgList.map((message, key) => {
+    if (message.sender === userName) {
+      return <Message {...message} key={key} />;
     }
-    var renderedMessages = messages.map((message, key) => {
-        return <Message {...message} key = {key} />;
-    });
-    var contacts = userList.map((user, key) => {
-        if(user.username !== userName) {
-            return <RenderContact {...user} key = {key} />
-        } 
-    });
-    return (
-        <div>
-        <div className="left-menu">
-        </div>
-        <div className="central-frame">
-            <div className="col-1 col-lg-3 g-0">{renderedMessages}</div>
-            <div>{contacts}</div>
-        </div>
-        </div>
-    );
+  });
+  var contacts = userList.map((user, key) => {
+    if (user.username !== userName) {
+      return <RenderContact {...user} key={key} />;
+    }
+  });
+  return (
+    <div>
+      <div className="central-frame">
+        <div className="left-menu">{contacts}</div>
+        <div className="chat">{renderedMessages}</div>
+      </div>
+    </div>
+  );
 }
 
 export default MainChat;
