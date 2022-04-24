2a98ade3e224a12127c812a9964e5a7c16a83aaa
YoadKaspi <yoad.kas88@gmail.com>
2022-04-24 23:55:07 +0300

    trying to fix designing

--- src/Chat/MainChat.js
+++ src/Chat/MainChat.js
@@ -7,42 +7,53 @@ import { useState } from "react";
 import "./MainChat.css";
 
 function MainChat() {
-  function renderNewMessage() {
-    var newMsg = { sender: userName, reciever: contactName, text: currMsg };
-    renderedMessages.push(<Message {...newMsg} />);
-  }
-  const { userName } = useParams();
-  const [contactName, setContactName] = useState("");
-  const [currMsg, setCurrMsg] = useState("");
-  var renderedMessages = msgList.map((message, key) => {
-    if (
-      (message.sender === userName && message.reciever === contactName) ||
-      (message.reciever === userName && message.sender === contactName)
-    ) {
-      return <Message {...message} key={key} />;
+    function renderNewMessage() {
+        var newMsg = { sender: userName, reciever: contactName, text: currMsg };
+        msgList.push(newMsg);
+        return { renderedMessages };
     }
-  });
 
-  var contacts = userList.map((user, key) => {
-    if (user.username !== userName) {
-      return (
-        <RenderContact {...user} key={key} contactState={setContactName} />
-      );
-    }
-  });
+    const { userName } = useParams();
+    const [contactName, setContactName] = useState("");
+    const [currMsg, setCurrMsg] = useState("");
 
-  return (
-    <div className="d-flex justify-content-start">
-      <div className="list-group"> {contacts} </div>
-      <div className="chat-frame chat-screen">
-        <div className="chat"> {renderedMessages} </div>
-        <div className="text-box">
-          <textarea onChange={(e) => setCurrMsg(e.target.value)}></textarea>
-          <button onClick={renderNewMessage()}>send</button>
+    var renderedMessages = msgList.map((message, key) => {
+        if (
+            (message.sender === userName && message.reciever === contactName) ||
+            (message.reciever === userName && message.sender === contactName)
+        ) {
+            return <Message {...message} key={key} />;
+        }
+    });
+
+    var contacts = userList.map((user, key) => {
+        if (user.username !== userName) {
+            return <RenderContact {...user} key={key} contactState={setContactName} />;
+        }
+    });
+
+    return (
+        <div className="d-flex justify-content-start">
+            <div>
+                <div className="contact-headline"> contact list </div>
+                <div className="list-group"> {contacts} </div>
+            </div>
+            <div className="chat-frame">
+                <div> {renderedMessages} </div>
+                <div className="textbox-block d-flex align-items-center">
+                    <div className=""></div>
+                    <div className="">
+                        <input className="form-control" value={currMsg}></input>
+                    </div>
+                    <div className="">
+                        <button className="btn btn-outline-dark" onClick={renderNewMessage()}>
+                            send
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default MainChat;
